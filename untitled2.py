306ead039c7f05172460649a0293590ed9169b61
brigytt <117694838+brigytt@users.noreply.github.com>
2022-11-18 00:40:21 -0500

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -12,7 +12,7 @@ def download_data():
    url="https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.xlsx"
    filename="Catalogo1960_2021.xlsx"
    urllib.request.urlretrieve(url,filename)
-   df=pd.read_csv('Catalogo1960_2021.xlsx')
+   df=pd.read_xlsx('Catalogo1960_2021.xlsx')
    return df
 c=download_data()
 st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' columnas')
