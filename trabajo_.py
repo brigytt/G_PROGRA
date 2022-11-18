4999be66964a5178771cf4fc3574305b16a36ff2
CalistroAguilar24 <118396586+CalistroAguilar24@users.noreply.github.com>
2022-11-17 23:12:30 -0500

    Update trabajo_.py

--- trabajo_.py
+++ trabajo_.py
@@ -1,4 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-st.title('CATALOGO SISMICO')
+import os 
+st.title('CATALOGO SISMICO 1960-2021 (IGP) ')
+mainpath = "drive/MyDrive/Trabajo"
+filename = "Catalogo1960_2021.xlsx"
+fullpath = os.path.join(mainpath,filename)
+df = pd.read_excel(fullpath)
+df.head(2000)
