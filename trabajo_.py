b43764f3ffde0636d4d95ff3bb778031851cc634
CalistroAguilar24 <118396586+CalistroAguilar24@users.noreply.github.com>
2022-11-18 00:17:24 -0500

    Update trabajo_.py

--- trabajo_.py
+++ trabajo_.py
@@ -3,8 +3,16 @@ import pandas as pd
 import numpy as np
 import os 
 st.title('CATALOGO SISMICO 1960-2021 (IGP) ')
-mainpath = "drive/MyDrive/Trabajo"
-filename = "Catalogo1960_2021.xlsx"
-fullpath = os.path.join(mainpath,filename)
-df = pd.read_excel(fullpath)
-df.head(2000)
+st.markdown("""Este dataset muestra los datos """)
+@st.experimental_memo
+def download_data():
+   url="https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.xlsx"
+   filename="Catalogo1960_2021.xlsx"
+   urllib.request.urlretrieve(url,filename)
+   df=pd.read_csv('Catalogo1960_2021.xlsx')
+   return df
+c=download_data()
+st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' columnas')
+st.dataframe(c)
+st.subheader("Características del Dataset")
+st.write(c.describe())
