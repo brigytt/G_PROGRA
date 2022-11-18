68e68c936193c1efbc918597b7d892bd1c742324
CalistroAguilar24 <118396586+CalistroAguilar24@users.noreply.github.com>
2022-11-18 01:01:59 -0500

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -1,18 +1,13 @@
 #$ pip install streamlit --upgrade
 import streamlit as st
-import pandas as pd
-import numpy as np
-import altair as alt
 import urllib.request
-import base64
 st.header("CATALOGO SISMICO 1960-2021 (IGP)")
-st.markdown("""Este dataset muestra los datos """)
 @st.experimental_memo
 def download_data():
-   url="https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.xlsx"
+   url="https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.csv"
    filename="Catalogo1960_2021.xlsx"
    urllib.request.urlretrieve(url,filename)
-   df=pd.read_xlsx('Catalogo1960_2021.xlsx')
+   df=pd.read_csv('Catalogo1960_2021.xlsx')
    return df
 c=download_data()
 st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' columnas')
