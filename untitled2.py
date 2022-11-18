5ff754e6e1937757b5a8f669c1b569dda1557d3d
CalistroAguilar24 <118396586+CalistroAguilar24@users.noreply.github.com>
2022-11-18 09:32:38 -0500

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -16,6 +16,13 @@ st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' c
 st.dataframe(c)
 st.subheader("Características del Dataset")
 st.write(c.describe())
+
+
+
 #url del archivo en formato raw
-url = ''
+url = 'https://raw.githubusercontent.com/brigytt/G_PROGRA/main/Catalogo1960_2021.csv'
+datos = pd.read_csv(url,sep= ',')
+st.line_chart(data=datos, x='FECHA_UTC', y='MAGNITUD')
+
+
 
