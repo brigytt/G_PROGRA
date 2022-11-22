c861c4faecee670e11435e42fc690d7662370999
brigytt <117694838+brigytt@users.noreply.github.com>
2022-11-21 20:00:44 -0500

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -4,8 +4,7 @@ import urllib.request
 import pandas as pd
 import numpy as np
 st.header("CATALOGO SISMICO 1960-2021 (IGP)")
-
-st.write("Contaminantes q") 
+@st.experimental_memo
 
 def download_data():
    url="https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.csv"
@@ -18,7 +17,7 @@ st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' c
 st.dataframe(c)
 st.subheader("Características del Dataset")
 st.write(c.describe())
-@st.experimental_memo
+
 #url del archivo en formato raw
 url = 'https://raw.githubusercontent.com/brigytt/G_PROGRA/main/Catalogo1960_2021.csv'
 datos = pd.read_csv(url,sep= ',')
