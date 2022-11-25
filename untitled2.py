9541d784b1476fbe06b3ec0dd1c0d496861a125f
CalistroAguilar24 <118396586+CalistroAguilar24@users.noreply.github.com>
2022-11-25 18:00:54 -0500

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -3,20 +3,37 @@ import streamlit as st
 import urllib.request
 import pandas as pd
 import numpy as np
+from streamlit_option_menu import option_menu
+
 st.header("CATALOGO SISMICO 1960-2021 (IGP)")
 @st.experimental_memo
 
-def download_data():
+with st.sidebar:
+   st.markdown("###")
+   st.sidebar.header('Programación avanzada')
+   selected = option_menu(
+      menu_title = 'Menú',
+      options = ['Inicio','Informe','Equipo'],
+      icons = ['house','book','book','people'],
+      menu_icon = 'cast',
+      default_index = 0,
+   )
+if selected == 'Inicio':
+   st.markdown("<h1 style ='text-aling: center'> Título:</h1>", unnsafe_allow_htm1= True)
+   st.markdown("---")
+   st.header("Dataset")
+   
+   def download_data():
    url="https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.csv"
    filename="Catalogo1960_2021.xlsx"
    urllib.request.urlretrieve(url,filename)
    df=pd.read_csv('Catalogo1960_2021.xlsx')
    return df
-c=download_data()
-st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' columnas')
-st.dataframe(c)
-st.subheader("Características del Dataset")
-st.write(c.describe())
+   c=download_data()
+   st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' columnas')
+   st.dataframe(c)
+   st.subheader("Características del Dataset")
+   st.write(c.describe())
 
 #url del archivo en formato raw
 url = 'https://raw.githubusercontent.com/brigytt/G_PROGRA/main/Catalogo1960_2021.csv'
