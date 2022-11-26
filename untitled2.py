523ccb7ae25ebf4f21928949016743acfecb3b72
CalistroAguilar24 <118396586+CalistroAguilar24@users.noreply.github.com>
2022-11-25 22:32:26 -0500

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -19,7 +19,7 @@ with st.sidebar:
       default_index = 0,
    )
 if selected == 'Inicio':
-   st.markdown("<h1 style ='text-align: center'> Titulo:</h1>", unsafe_allow_html= True)
+   st.markdown("<h1 style ='text-align: center'> CATALOGO SISMICO 1960-2021 (IGP):</h1>", unsafe_allow_html= True)
    st.markdown("---")
    st.header("Dataset")
    
