7d327f402ee954d6bdc0392343092e668db4fbfd
CalistroAguilar24 <118396586+CalistroAguilar24@users.noreply.github.com>
2022-11-25 22:30:25 -0500

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -35,7 +35,7 @@ if selected == 'Inicio':
    st.subheader("Características del Dataset")
    st.write(c.describe())
 
-url del archivo en formato raw
+#url del archivo en formato raw
 url = 'https://raw.githubusercontent.com/brigytt/G_PROGRA/main/Catalogo1960_2021.csv'
 datos = pd.read_csv(url,sep= ',')
 st.line_chart(data=datos, x='FECHA_UTC', y='MAGNITUD')
