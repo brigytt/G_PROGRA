c48b166454d4b2a271330a98e7a0ead6de623bad
CalistroAguilar24 <118396586+CalistroAguilar24@users.noreply.github.com>
2022-11-27 22:44:41 -0500

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -9,8 +9,9 @@ from PIL import Image
 #@st.experimental_memo
 
 with st.sidebar:
-   st.markdown("CATALOGO SISMICO 1960-2021 (IGP)")
    st.sidebar.header('Programación avanzada')
+   image = Image.open('Logo_Oficial (1).png')
+   st.image(image,use_column_width=True)
    selected = option_menu(
       menu_title = 'Menú',
       options = ['Inicio','Informe','Equipo'],
@@ -19,10 +20,10 @@ with st.sidebar:
       default_index = 0,
    )
 if selected == 'Inicio':
-   st.markdown("<h1 style ='text-align: center'> CATÁLOGO SÍSMICO 1960-2021 (IGP):</h1>", unsafe_allow_html= True)
+   st.markdown("<h1 style ='text-align: center'> Introducción:</h1>", unsafe_allow_html= True)
    st.markdown("---")
-   st.header("Los desastres naturales han acompañado el desarrollo de la humanidad a lo largo de la historia; por ello, rescatar y analizar a los sismos desde la historia ayuda a comprender no sólo las acciones humanas en torno a ellos, sino también a descifrar las características y patrones de comportamiento de la actividad sísmica, lo que puede ayudar a los sismólogos, geólogos y otros especialistas a elaborar con mayores datos y precisión, los mapas de riesgo.")
-   st.header(" Además, tener conocimiento sobre estos fenómenos nos permitirá construir una sociedad preventiva, la vulnerabilidad física y la vulnerabilidad social nos ayudará a contar con herramientas –como simulacros y mochilas de emergencia– para estar preparados ante lo que podemos esperar en un evento real.")          
+   st.write("Los desastres naturales han acompañado el desarrollo de la humanidad a lo largo de la historia; por ello, rescatar y analizar a los sismos desde la historia ayuda a comprender no sólo las acciones humanas en torno a ellos, sino también a descifrar las características y patrones de comportamiento de la actividad sísmica, lo que puede ayudar a los sismólogos, geólogos y otros especialistas a elaborar con mayores datos y precisión, los mapas de riesgo.")
+   st.write(" Además, tener conocimiento sobre estos fenómenos nos permitirá construir una sociedad preventiva, la vulnerabilidad física y la vulnerabilidad social nos ayudará a contar con herramientas –como simulacros y mochilas de emergencia– para estar preparados ante lo que podemos esperar en un evento real.")          
            
 
    def download_data():
@@ -36,13 +37,35 @@ if selected == 'Inicio':
    st.dataframe(c)
    st.subheader("Características del Dataset")
    st.write(c.describe())
-   
-image = Image.open('imagen 1.jpg')
-
-#url del archivo en formato raw
-url = 'https://raw.githubusercontent.com/brigytt/G_PROGRA/main/Catalogo1960_2021.csv'
-datos = pd.read_csv(url,sep= ',')
-st.line_chart(data=datos, x='FECHA_UTC', y='MAGNITUD')
-
-
-
+   #url del archivo en formato raw
+   url = 'https://raw.githubusercontent.com/brigytt/G_PROGRA/main/Catalogo1960_2021.csv'
+   datos = pd.read_csv(url,sep= ',')
+   st.line_chart(data=datos, x='FECHA_UTC', y='MAGNITUD')
+   image = Image.open('imagen 1.jpg')
+   st.image(image,use_column_width=True)
+if selected == 'Informe':
+   st.markdown("<h1 style ='text-align: center'> CATÁLOGO SÍSMICO 1960-2021 (IGP):</h1>", unsafe_allow_html= True)
+   st.markdown("---")
+   selected_year=st.sidebar.selectbox('Fecha', list(reversed(range(1960,2021))))
+   def download_data():
+      url="https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.csv"
+      filename="Catalogo1960_2021.xlsx"
+      urllib.request.urlretrieve(url,filename)
+      df=pd.read_csv('Catalogo1960_2021.xlsx')
+      filt=(df["FECHA_UTC"] == selected_year)
+      df[filt]
+   download_data()
+if selected == 'Equipo':
+   st.markdown("<h1 style ='text-align: center'> ¿Quiénes somos?:</h1>", unsafe_allow_html= True)
+   st.markdown("---")
+   st.write('Somos un grupo de estudiantes del V ciclo de la carrera de Ingeniería Ambiental de la Universidad Peruano Cayetano Heredia (UPCH). Mediante el procesamiento y visualización de datos ')
+   col1, col2, col3= st.columns(3)
+   image1 = Image.open('b9fd20744ad6f008787ffed46a0b7149--s-cartoons-bart-simpson.jpg')
+   col1.header("Miguel Calistro Aguilar")
+   col1.image(image1, use_column_width=True)
+   image2 = Image.open('273-2736237_20-lisa-simpson-tumblr-listening-to-headphones-pictures.png')
+   col2.header("Brigytt Contreras Melgar")
+   col2.image(image2, use_column_width=True)
+   image3 = Image.open('mqdefault.jpg')
+   col3.header("Daniel Chamorro Grados")
+   col3.image(image3, use_column_width=True)
