61209af2ad7c38ac0a930a091680f1faab657c4f
brigytt <117694838+brigytt@users.noreply.github.com>
2022-12-02 18:24:37 -0500

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -53,8 +53,6 @@ if selected == 'Informe':
       urllib.request.urlretrieve(url,filename)
       df=pd.read_csv('Catalogo1960_2021.xlsx')
       filt=(df["FECHA_UTC"] == selected_year)
-      datos = pd.read_csv(url,sep= ',')
-      filt=(df[filt] == datos)
       return df[filt] 
    x=download_data(selected_year)
    st.table(x)
