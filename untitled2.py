765035687caabe0197760617bcfa97184e0d3ef9
brigytt <117694838+brigytt@users.noreply.github.com>
2022-12-03 14:04:06 -0500

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -43,7 +43,8 @@ if selected == 'Inicio':
 if selected == 'Informe':
    st.markdown("<h1 style ='text-align: center'> CATÁLOGO SÍSMICO 1960-2021 (IGP):</h1>", unsafe_allow_html= True)
    st.markdown("---")
-   #url del archivo en formato raw
+   st.subheader('FECHA_UTC vs. EPICENTRO')
+   st.write('La grafica indica.....')
    url = 'https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.csv'
    datos = pd.read_csv(url,sep= ',')
    st.line_chart(data=datos, x='FECHA_UTC', y='EPICENTRO')
