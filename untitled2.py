9ff8afb8b1dbc8d1c391c27a8d64dd7c20f66480
brigytt <117694838+brigytt@users.noreply.github.com>
2022-12-03 15:17:53 -0500

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -66,7 +66,7 @@ if selected == 'Informe':
       df_year = grouped.get_group(year)
       return df_year
 
-   data_by_year=load_data(str(selected_year))
+   data_by_year= load_data(str(selected_year))
 	
    sorted_unique_district = sorted(data_by_year.DEPARTAMENTO.unique())
    selected_district=st.sidebar.multiselect('Departamento', sorted_unique_district, sorted_unique_district)
