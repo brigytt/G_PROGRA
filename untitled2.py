1ad8dc5c87d7db4eaa623025fddf0442f690111c
CalistroAguilar24 <118396586+CalistroAguilar24@users.noreply.github.com>
2022-12-04 02:03:04 -0500

    Update untitled2.py

--- untitled2.py
+++ untitled2.py
@@ -22,7 +22,7 @@ with st.sidebar:
 if selected == 'Inicio':
    st.markdown("<h1 style ='text-align: center'> CATALOGO SISMICO 1960-2021 (IGP) </h1>", unsafe_allow_html= True)
    st.markdown("---")
-   st.write('La base de datos sobre la actividad sísmica en el país fue realizada por el Instituto Geofísico del Perú (IGP) desde el año de 1960 hasta el 2021. El IGP es la institución responsable del monitoreo de la actividad sísmica del país, y contiene todos aquellos sismos percibidos por la población y registrados por la Red Sísmica Nacional desde 1960, fecha en la que se inicia la vigilancia instrumental de la sismicidad en el Perú.')
+   st.info('La base de datos sobre la actividad sísmica en el país fue realizada por el Instituto Geofísico del Perú (IGP) desde el año de 1960 hasta el 2021. El IGP es la institución responsable del monitoreo de la actividad sísmica del país, y contiene todos aquellos sismos percibidos por la población y registrados por la Red Sísmica Nacional desde 1960, fecha en la que se inicia la vigilancia instrumental de la sismicidad en el Perú.')
    st.write('¿Por qué es importante saber sobre los datos de los sismos?')
    col1, col2 =st.columns(2)
    image = Image.open('imagen 1.jpg')
@@ -43,52 +43,159 @@ if selected == 'Inicio':
 if selected == 'Informe':
    st.markdown("<h1 style ='text-align: center'> CATÁLOGO SÍSMICO 1960-2021 (IGP):</h1>", unsafe_allow_html= True)
    st.markdown("---")
-   selected_year=st.sidebar.selectbox('FECHA_UTC', list(reversed(range(1960,2022))))
-   def download_data():
-      url="https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.csv"
-      filename="Catalogo1960_2021.xlsx"
-      urllib.request.urlretrieve(url,filename)
-      df=pd.read_csv('Catalogo1960_2021.xlsx')
-      return df
-   c=download_data()
-   st.write('Dimensiones: ' + str(c.shape[0]) + ' filas y ' + str(c.shape[1]) + ' columnas')
-   st.dataframe(c)
-   st.subheader("Características del Dataset")
-   st.write(c.describe())
+   #DATOS POR FECHA
    
-   def load_data(year):
-      df=download_data()
-      df=df.astype({'FECHA_UTC':'str'})
-      df['MAGNITUD']= pd.to_numeric(df['MAGNITUD'])
-      df['LATITUD']= pd.to_numeric(df['LATITUD'])
-      df['LONGITUD']= pd.to_numeric(df['LONGITUD'])
-      grouped = df.groupby(df.FECHA_UTC)
-      df_year = grouped.get_group(year)
-      return df_year
+   #DATOS POR DEPARTAMENTO
 
-   data_by_year= load_data(str(selected_year))
-	
-   sorted_unique_district = sorted(data_by_year.DEPARTAMENTO.unique())
-   selected_district=st.sidebar.multiselect('Departamento', sorted_unique_district, sorted_unique_district)
+   opcion_dataset = st.selectbox('Eliga el Departamento',('SELECCIONAR','AMAZONAS','ANCASH','APURIMAC','AREQUIPA','AYACUCHO','UCAYALI','TUMBES','TACNA','CAJAMARCA','CALLAO','SAN MARTIN','PUNO','CUZCO','PIURA','PASCO','HUANCAVELICA','HUANUCO','ICA','JUNIN','LA LIBERTAD','LAMBAYEQUE','LIMA','LORETO','MADRE DE DIOS','MAR'))
+   estado = '-'
+   if opcion_dataset == 'SELECCIONAR':
+      df_visualizacion = None
+   datos_Amazonas= pd.read_csv('Amazonas.csv')
+   if opcion_dataset == 'AMAZONAS':
+      df_visualizacion = datos_Amazonas
+      st.dataframe(df_visualizacion)
+   datos_Ancash= pd.read_csv('Ancash.csv')
+   if opcion_dataset == 'ANCASH':
+      df_visualizacion = datos_Ancash
+      st.dataframe(df_visualizacion)
+   datos_Apurimac= pd.read_csv('Apurimac.csv')
+   if opcion_dataset == 'APURIMAC':
+      df_visualizacion = datos_Apurimac
+      st.dataframe(df_visualizacion)
+   datos_Arequipa= pd.read_csv('Arequipa.csv')
+   if opcion_dataset == 'AREQUIPA':
+      df_visualizacion = datos_Arequipa
+      st.dataframe(df_visualizacion)
+   datos_Ayacucho= pd.read_csv('Ayacucho.csv')
+   if opcion_dataset == 'AYACUCHO':
+      df_visualizacion = datos_Ayacucho
+      st.dataframe(df_visualizacion)
+   datos_Cajamarca= pd.read_csv('Cajamarca.csv')
+   if opcion_dataset == 'CAJAMARCA':
+      df_visualizacion = datos_Cajamarca
+      st.dataframe(df_visualizacion)
+   datos_Callao= pd.read_csv('Callao.csv')
+   if opcion_dataset == 'CALLAO':
+      df_visualizacion = datos_Callao
+      st.dataframe(df_visualizacion)
+   datos_Cuzco= pd.read_csv('Cuzco.csv')
+   if opcion_dataset == 'CUZCO':
+      df_visualizacion = datos_Cuzco
+      st.dataframe(df_visualizacion)
+   datos_Huancavelica= pd.read_csv('Huancavelica.csv')
+   if opcion_dataset == 'HUANCAVELICA':
+      df_visualizacion = datos_Huancavelica
+      st.dataframe(df_visualizacion)
+   datos_Huanuco= pd.read_csv('Huanuco.csv')
+   if opcion_dataset == 'HUANUCO':
+      df_visualizacion = datos_Huanuco
+      st.dataframe(df_visualizacion)
+   datos_Ica= pd.read_csv('Ica.csv')
+   if opcion_dataset == 'ICA':
+      df_visualizacion = datos_Ica
+      st.dataframe(df_visualizacion)
+   datos_Junin= pd.read_csv('Junin.csv')
+   if opcion_dataset == 'JUNIN':
+      df_visualizacion = datos_Junin
+      st.dataframe(df_visualizacion)
+   datos_LaLibertad= pd.read_csv('La Libertad.csv')
+   if opcion_dataset == 'LA LIBERTAD':
+      df_visualizacion = datos_LaLibertad
+      st.dataframe(df_visualizacion)
+   datos_Lambayeque= pd.read_csv('Lambayeque.csv')
+   if opcion_dataset == 'LAMBAYEQUE':
+      df_visualizacion = datos_Lambayeque
+      st.dataframe(df_visualizacion)
+   datos_Lima= pd.read_csv('Lima.csv')
+   if opcion_dataset == 'LIMA':
+      df_visualizacion = datos_Lima
+      st.dataframe(df_visualizacion)
+   datos_Loreto= pd.read_csv('Loreto.csv')
+   if opcion_dataset == 'LORETO':
+      df_visualizacion = datos_Loreto
+      st.dataframe(df_visualizacion)
+   datos_MadredeDios= pd.read_csv('Madre de Dios.csv')
+   if opcion_dataset == 'MADRE DE DIOS':
+      df_visualizacion = datos_MadredeDios
+      st.dataframe(df_visualizacion)
+   datos_Mar= pd.read_csv('Mar.csv')
+   if opcion_dataset == 'MAR':
+      df_visualizacion = datos_Mar
+      st.dataframe(df_visualizacion)
+   datos_Pasco= pd.read_csv('Pasco.csv')
+   if opcion_dataset == 'PASCO':
+      df_visualizacion = datos_Pasco
+      st.dataframe(df_visualizacion)
+   datos_Piura= pd.read_csv('Piura.csv')
+   if opcion_dataset == 'PIURA':
+      df_visualizacion = datos_Piura
+      st.dataframe(df_visualizacion)
+   datos_Puno= pd.read_csv('Puno.csv')
+   if opcion_dataset == 'PUNO':
+      df_visualizacion = datos_Puno
+      st.dataframe(df_visualizacion)
+   datos_SanMartin= pd.read_csv('San Martin.csv')
+   if opcion_dataset == 'SAN MARTIN':
+      df_visualizacion = datos_SanMartin
+      st.dataframe(df_visualizacion)
+   datos_Tacna= pd.read_csv('Tacna.csv')
+   if opcion_dataset == 'TACNA':
+      df_visualizacion = datos_Tacna
+      st.dataframe(df_visualizacion)
+   datos_Tumbes= pd.read_csv('Tumbes.csv')
+   if opcion_dataset == 'TUMBES':
+      df_visualizacion = datos_Tumbes
+      st.dataframe(df_visualizacion)
+   datos_Ucayali= pd.read_csv('Ucayali.csv')
+   if opcion_dataset == 'UCAYALI':
+      df_visualizacion = datos_Ucayali
+      st.dataframe(df_visualizacion)
    
-   unique_contaminant=['MAGNITUD', 'LATITUD', 'LONGITUD']
-   selected_contaminant=st.sidebar.multiselect('Contaminante', unique_contaminant, unique_contaminant)
- 
-   df_selected=data_by_year[(data_by_year.DEPARTAMENTO.isin(selected_district))]
-   def remove_columns(dataset, cols):
-      return dataset.drop(cols, axis=1)
+   #DATOS POR PAÍS
+   opcion_dataset = st.selectbox('Eliga el país',('SELECCIONAR','BOLIVIA','BRASIL','CHILE','COLOMBIA','ECUADOR'))
+   estado = '-'
+   if opcion_dataset == 'SELECCIONAR':
+      df_visualizacion = None
+   datos_Bolivia= pd.read_csv('Bolivia.csv')
+   if opcion_dataset == 'BOLIVIA':
+      df_visualizacion = datos_Bolivia
+      st.dataframe(df_visualizacion)
+   datos_Brasil= pd.read_csv('Brasil.csv')
+   if opcion_dataset == 'BRASIL':
+      df_visualizacion = datos_Brasil
+      st.dataframe(df_visualizacion)
+   datos_Chile= pd.read_csv('Chile.csv')
+   if opcion_dataset == 'CHILE':
+      df_visualizacion = datos_Chile
+      st.dataframe(df_visualizacion)
+   datos_Colombia= pd.read_csv('Colombia.csv')
+   if opcion_dataset == 'COLOMBIA':
+      df_visualizacion = datos_Colombia
+      st.dataframe(df_visualizacion)
+   datos_Ecuador= pd.read_csv('Ecuador.csv')
+   if opcion_dataset == 'ECUADOR':
+      df_visualizacion = datos_Ecuador
+      st.dataframe(df_visualizacion)
 
-   cols=np.setdiff1d(unique_contaminant, selected_contaminant)
-   st.subheader('Mostrar data de distrito(s) y contaminante(s) seleccionado(s)')
-   data=remove_columns(df_selected, cols)
-   st.write('Dimensiones: ' + str(data.shape[0]) + ' filas y ' + str(data.shape[1]) + ' columnas')
-   st.dataframe(data)
+   
+   
+   #url archivo raw
+   url= 'https://www.datosabiertos.gob.pe/sites/default/files/Catalogo1960_2021.csv'
+   datos=pd.read_csv(url, sep=',')
+   st.subheader('Gráfico de Epicentro vs Fecha_UTC')
+   st.write('')
+   st.line_chart(data=datos, x='EPICENTRO', y='FECHA_UTC')
+   st.subheader('Gráfico de Magnitud vs Departamento')
+   st.write('')
+   st.line_chart(data=datos, x='MAGNITUD', y='DEPARTAMENTO')
+   st.subheader('Gráfico de Fecha_UTC vs Departamento')
+   st.write('')
+   st.line_chart(data=datos, x='FECHA_UTC', y='DEPARTAMENTO')
+   st.subheader('Gráfico de Profundidad vs Departamento')
+   st.write('')
+   st.line_chart(data=datos, x='PROFUNDIDAD', y='DEPARTAMENTO')
 	
-   set_departamentos = np.sort(c['DEPARTAMENTO'].dropna().unique())
-   #Seleccion del departamento
-   opcion_departamento = st.selectbox('Selecciona un departamento', set_departamentos)
-   df_departamentos = c[c['DEPARTAMENTO'] == opcion_departamento]
-   num_filas = len(df_departamentos.axes[0]) 
 
 if selected == 'Equipo':
    st.markdown("<h1 style ='text-align: center'> ¿Quiénes somos?:</h1>", unsafe_allow_html= True)
@@ -104,3 +211,4 @@ if selected == 'Equipo':
    image3 = Image.open('WhatsApp Image 2022-11-28 at 19.18.22.jpeg')
    col3.header("Daniel Chamorro Grados")
    col3.image(image3, use_column_width=True)
+   
